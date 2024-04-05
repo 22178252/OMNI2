2d09e98ed48561d617973a241fa811753f81b5b1
22178252 <165087597+22178252@users.noreply.github.com>
2024-04-05 01:49:42 -0800

    Initial commit

---
+++ Src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String result = methode(8);
+        System.out.println(result);
+    }
+
+    public static String methode(int getal) {
+        String tekst = "";
+        for (int i = 0; i < getal; i++) {
+            tekst += "hello.\n";
+        }
+        return tekst;
+    }
+}
